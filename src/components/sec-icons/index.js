9774dd3e56f7a359a5d288e3a9c5cc01dca3f4a0
Alexsandro Martins <alexsandromart2002@protonmail.com>
2022-03-22 18:41:37 -0300

    Mobile

--- src/components/sec-icons/index.js
+++ src/components/sec-icons/index.js
@@ -1,21 +1,27 @@
 import { Card, Content, Img, H3 } from "./style";
-import Icon01 from "../img/icon01.svg";
-import Icon02 from "../img/icon02.svg";
-import Icon03 from "../img/icon03.svg";
+import Icon from "../img/icon.svg";
+import Icon2 from "../img/icon2.svg";
+import Icon3 from "../img/icon3.svg";
 
 function Icons() {
   return (
     <>
       <Card>
-        <Img src={Icon01} />
+        <Img src={Icon} />
         <Content>
           <H3>Foguetes com a mais alta tecnologia e conforto.</H3>
         </Content>
       </Card>
       <Card>
-        <Img src={Icon02} />
+        <Img src={Icon2} />
         <Content>
           <H3>Mais de 100 missões consecutivas com sucesso.</H3>
+        </Content>
+      </Card>
+      <Card>
+        <Img src={Icon3} />
+        <Content>
+          <H3>Experiencia única e exclusiva.</H3>
         </Content>
       </Card>
     </>
